181af8c5d89dcf9d0f9f9ae0a7201d8dbf2bcab7
BaekGeunYoung <dvmflstm@naver.com>
2019-12-10 11:52:45 +0900

    가독성 / 생산성 향상을 위해 핸들러 함수 이름을 모두 handle로 통일(overloading). errorResponse 객체를 ResponseEntity로 감싸서 리턴하니까 의도한대로 에러 핸들링이 잘 됨. PasswordNotMatchedException handler 추가.

--- src/main/kotlin/com/practice/embeddable_password/config/controller/ErrorExceptionController.kt
+++ src/main/kotlin/com/practice/embeddable_password/config/controller/ErrorExceptionController.kt
@@ -3,49 +3,49 @@ package com.practice.embeddable_password.config.controller
 import com.practice.embeddable_password.controller.response.ErrorResponse
 import com.practice.embeddable_password.exception.ErrorCode
 import com.practice.embeddable_password.exception.PasswordFailedExceededException
+import com.practice.embeddable_password.exception.PasswordNotMatchedException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
-class ErrorExceptionController : ResponseEntityExceptionHandler() {
+class ErrorExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(PasswordFailedExceededException::class)
-    fun handlePasswordFailedExceededException(e: PasswordFailedExceededException): ErrorResponse {
+    fun handle(e: PasswordFailedExceededException): ResponseEntity<ErrorResponse> {
         val errorCode = ErrorCode.PASSWORD_FAILED_COUNT_EXCEEDED
+        val errorResponse = ErrorResponse.of(errorCode)
 
-        return ErrorResponse(
-                message = errorCode.message,
-                code = errorCode.code,
-                status = errorCode.status
-        )
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [(BadCredentialsException::class)])
-    fun handleBadeCredentialsException(e: BadCredentialsException): ErrorResponse {
+    fun handle(e: BadCredentialsException): ResponseEntity<ErrorResponse> {
         val errorCode = ErrorCode.AUTHENTICATION_FAILED
+        val errorResponse = ErrorResponse.of(errorCode)
 
-        return ErrorResponse(
-                message = errorCode.message,
-                code = errorCode.code,
-                status = errorCode.status
-        )
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(UsernameNotFoundException::class)
-    fun handleUsernameNotFoundException(e: UsernameNotFoundException): ErrorResponse {
-        val errorCode = ErrorCode.ACCOUNT_NOT_FOUND
+    @ExceptionHandler(value = [(PasswordNotMatchedException::class)])
+    fun handle(e: PasswordNotMatchedException): ResponseEntity<ErrorResponse> {
+        val errorCode = ErrorCode.AUTHENTICATION_FAILED
+        val details = listOf(ErrorResponse.ErrorDetail("current fail count : ${e.failedCount}"))
 
-        return ErrorResponse(
-                message = errorCode.message,
-                code = errorCode.code,
-                status = errorCode.status
-        )
+        val errorResponse = ErrorResponse.of(errorCode, details)
+
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(UsernameNotFoundException::class)
+    fun handle(e: UsernameNotFoundException): ResponseEntity<ErrorResponse> {
+        val errorCode = ErrorCode.ACCOUNT_NOT_FOUND
+        val errorResponse = ErrorResponse.of(errorCode)
+
+        return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
     }
 }
